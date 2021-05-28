440f8a303a10555daa5fc7092eaf7522c8e7c745
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-05-28 17:31:56 -0500

    initial commit

---
+++ leetcode/1.two-sum.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+/* 
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+var twoSum = function (nums, target) {
+};
+
+// @lc code=end
